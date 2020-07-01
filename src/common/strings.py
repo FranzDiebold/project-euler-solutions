a71d622a2d43f6ed4a1bbae6e70766c2afaba7cf
Franz Diebold <franz.diebold@tum.de>
2020-07-01 18:03:13 +0200

    Add solution for problem 59.

--- src/common/strings.py
+++ src/common/strings.py
@@ -9,3 +9,8 @@ def get_alphabetical_value(string: str) -> int:
     Example: 'COLIN' -> 53 = 3 + 15 + 12 + 9 + 14
     """
     return sum([(ord(char) - ord('a') + 1) for char in string.lower()])
+
+
+def sum_ascii_values(text: str) -> int:
+    """Sum the ASCII values of the given text `text`."""
+    return sum(ord(character) for character in text)
